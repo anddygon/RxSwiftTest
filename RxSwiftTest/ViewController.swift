93ea13d155977f21b6de0d51efdd6b363a2897fd
xiaoP <anddygong@gmail.com>
2017-03-15 18:30:36 +0800

    定时器相关

--- RxSwiftTest/ViewController.swift
+++ RxSwiftTest/ViewController.swift
@@ -7,12 +7,40 @@
 //
 
 import UIKit
+import RxSwift
+import RxCocoa
 
 class ViewController: UIViewController {
+    
+    let bag = DisposeBag()
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+//        //每隔一秒发送一个int
+//        Observable<Int>.interval(1, scheduler: MainScheduler.instance)
+//            .debug("interval", trimOutput: true)
+//            .bindNext { (i: Int) in
+//                print("interval \(i)")
+//            }
+//            .addDisposableTo(bag)
+//        
+//        //5秒后发送一次 然后发送complete
+//        Observable<Int>.timer(5, scheduler: MainScheduler.instance)
+//            .debug("timer", trimOutput: true)
+//            .bindNext { (i: Int) in
+//                print("timer \(i)")
+//            }
+//            .addDisposableTo(bag)
+        
+//        //5秒后发送第一个值，然后每隔1秒发送下一个值
+//        Observable<Int>.timer(5, period: 1, scheduler: MainScheduler.instance)
+//            .debug("timer period", trimOutput: true)
+//            .bindNext { (i: Int) in
+//                print("timer \(i)")
+//            }
+//            .addDisposableTo(bag)
     }
-
+    
 }
 
