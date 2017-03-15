790e35a7e20c3aec3c6b920db697abfa4431449d
xiaoP <anddygong@gmail.com>
2017-03-15 17:50:20 +0800

    .

---
+++ RxSwiftTest/AppDelegate.swift
@@ -0,0 +1,23 @@
+//
+//  AppDelegate.swift
+//  RxSwiftTest
+//
+//  Created by xiaoP on 2017/3/15.
+//  Copyright © 2017年 Chicv. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+        return true
+    }
+
+
+}
+
---
+++ RxSwiftTest/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  RxSwiftTest
+//
+//  Created by xiaoP on 2017/3/15.
+//  Copyright © 2017年 Chicv. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+}
+
